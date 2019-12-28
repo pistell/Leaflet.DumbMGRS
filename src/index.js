6e9caca726d5c86ce47757e20aa86dd009588996
James Pistell <jamespistell@gmail.com>
2019-12-27 21:10:44 -0500

    leaflet, es6, babel, eslint boilerplate done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import "./styles.scss";
-console.log("hello world!");
+import './styles.scss';
+import * as mgrs from 'mgrs';
+import L from 'leaflet';
+
+console.log('hello world!');
+console.log(L);
+
+
+const map = L.map('map').setView([40.123503280320634, -77.74869918823244], 14);
+L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  id: 'osm_map',
+}).addTo(map);
